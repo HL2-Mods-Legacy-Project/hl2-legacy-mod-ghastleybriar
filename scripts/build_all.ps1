0aee7a403eecaaf68aa7f68f4354d89aaaed307a
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 20:01:08 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "Ghastleybriar" `
+  -ModName "The Ghastleybriar Zoo Incident" `
+  -ModFolder "Ghastleybriar" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/ghastleybriar/Ghastleybriar+materials_fixed.zip" `
+  -ModBaseFilesUrlHash "f1e924215cdf35944978fd370b09ba371b63c54971d51a5dc22e6e833e668f8d" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
